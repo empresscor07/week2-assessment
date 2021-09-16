70bb0e9d502399aef64c5aa46b317f6b1f7ced73
empresscor07 <burns.corey1986@gmail.com>
2021-09-16 15:52:58 -0500

    Updates 9/15/21 5:30pm

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var fade = document.getElementById("fade-effect");
+
+$(document).ready(function (){
+    $("#fade-effect").mouseenter(function (){
+        $("#fade-in").fadeIn("slow");
+    })
+})
