febf6d59fe9e5e6bfbd0a661d17f8463b7913494
empresscor07 <burns.corey1986@gmail.com>
2021-09-16 21:12:36 -0500

    Updates 9/16/21 4:30pm

--- js/script.js
+++ js/script.js
@@ -1,7 +1,47 @@
-var fade = document.getElementById("fade-effect");
+var btn = document.getElementById("btn");
+var listingCont = document.getElementById("listing-cont");
+const url = "http://3.21.225.172:8080/api/realestate/getByRowAmount?rows=5";
 
-$(document).ready(function (){
-    $("#fade-effect").mouseenter(function (){
-        $("#fade-in").fadeIn("slow");
-    })
-})
+function requestListings() {
+    console.log("hello1");
+    var ourRequest = new XMLHttpRequest();
+    ourRequest.open('GET', url)
+    ourRequest.onload =
+        function () {
+        console.log("hello2");
+        var ourData = JSON.parse(ourRequest.responseText);
+        renderHTML(ourData);
+    };
+        ourRequest.send();
+    }
+function updateData() {
+
+}
+function renderHTML(data) {
+    var htmlString = "";
+    for (i = 0; i < data.length; i++) {
+        htmlString += "<p>" + data[i].price + "</p><br>";
+        console.log(htmlString);
+        listingCont.insertAdjacentHTML('beforeend', htmlString);
+    }
+   /*     $.each(data, function (i, item) {
+            var html =
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>"
+                "<td>" + item.fname + "</td>";
+            $("<tr/>").html(html).appendTo("#records");
+        }); */
+};
+updateData();
