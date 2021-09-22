0af78f62a1b2785513691a8763cd880ca89fda98
empresscor07 <burns.corey1986@gmail.com>
2021-09-22 15:15:46 -0500

    FINAL

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ let correctObject;
 let selectedHouse;
 
 //FUNCTIONS FOR INDEX PAGE (HOME PAGE)
+//function to pull data from server on page load event
 function displayTopHouses() {
     const ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', url);
@@ -17,16 +18,18 @@ function displayTopHouses() {
         //use json function to read the text from the response and split into a javascript array
         function () {
             let ourData = JSON.parse(ourRequest.responseText);
-            //call function to get listing with highest price, biggest square footage, most rooms and most bathrooms and insert correct IMGURL into img element
-
+            //call function to get listing with highest price, biggest square footage,
+            // most rooms and most bathrooms and insert correct IMGURL into img element
+            // for a fading image slide show of houses with top values
             document.getElementById("expensive").src = bestHouseFinder(ourData, "price");
             document.getElementById("big").src = bestHouseFinder(ourData, "sqft");
             document.getElementById("rooms").src = bestHouseFinder(ourData, "beds");
             document.getElementById("baths").src = bestHouseFinder(ourData, "baths");
         };
-
 }
 
+//function to find the top values throughout the entire array of houses
+//uses a loop to iterate through each house object
 function bestHouseFinder(arrayOfHouseObjects, searchParameter) {
     //Initializing the number to beat as zero
     let numberToBeat = 0;
@@ -34,14 +37,9 @@ function bestHouseFinder(arrayOfHouseObjects, searchParameter) {
     let bestHouse = {};
     //This for loop iterates over the array of objects given to it
     for (let house of arrayOfHouseObjects) {
-        //Let's take this if statement word by word
-        //Number with a capital N is a constructor that converts the value of numbers as a string into an int type
         //Object.values(house) returns all the values in the house object in an array
         //Object.keys(house) returns all of the keys in the house object in an array
         //Object.keys(house).indexOf(searchParameter) returns the index of the keyword
-        //In this case it will be set to the variable searchParameter on function call
-        //If you call this function as bestHouseFinder(bobsArray, "price")
-        //The below expression would be equal to house.price
         if (Number(Object.values(house)[Object.keys(house).indexOf(searchParameter)]) > numberToBeat) {
             //If the house.price is greater than the number to beat, this will dod two things
             //It will change the number to beat to house.price, then, it will set the best house to the house object
@@ -51,17 +49,16 @@ function bestHouseFinder(arrayOfHouseObjects, searchParameter) {
         //It will then iterate through the array again and if house.price > the number to beat, the if statement will fire off again
         //If not it will do nothing
     }
-    //After the for loop has run 50 times, or whatever the length of the array is
     //It will return the house that had the highest value with the parameter specified
     console.log(urlShort + bestHouse.imageurl);
     return urlShort + bestHouse.imageurl;
 }
 
 //FUNCTIONS FOR LISTINGS PAGE
-
 //function to pull data from server when search button clicked
 function requestListings() {
-    //request user entered parameter to determine low price and high price -- if fields left empty keep values as set above
+    //request user entered parameter to determine low price and high price
+    // if fields left empty keep values as set above
     if (document.getElementById("min_price").value !== "") {
         minPrice = document.getElementById("min_price").value
     }
@@ -70,22 +67,20 @@ function requestListings() {
     }
     //clears out any previously displayed data
     document.getElementById("card-container").innerHTML = "";
-
+    //request to server
     const ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', url);
+    ourRequest.send();
     ourRequest.onload =
         //use json function to read the text from the response and split into a javascript array
         function () {
             let ourData = JSON.parse(ourRequest.responseText);
-            //scan through data to determine where there is a match in price
-            //calls function that generates table with array ourData
             generateCards(ourData);
         };
-    ourRequest.send();
 }
 
 //function to create and fill card elements with results
-
+//takes array of houses as an argument
 function generateCards(data) {
 
     for (let object of data) {
@@ -112,7 +107,7 @@ function generateCards(data) {
             let imgURL = urlShort + object.imageurl;
             pic.src = imgURL;
 
-            pic.className = 'card-img-top img-fluid mx-auto d-block stretched-link';
+            pic.className = 'card-img-top img-fluid';
             pic.id = object.imageurl;
             pic.onclick = function () {
                 imgId = this.id;
@@ -130,25 +125,25 @@ function generateCards(data) {
     }
 }
 
-//FUNCTIONS FOR DETAILS PAGE
-
+//FUNCTIONS FOR LISTINGS PAGE WHEN DISPLAYING DETAILS
+//function to pull data from server when an image from listings page is clicked
 function displaySelectedHouse() {
     document.getElementById("card-container").innerHTML = "";
     const ourRequest = new XMLHttpRequest();
     ourRequest.open('GET', url);
+    ourRequest.send();
     ourRequest.onload =
         //use json function to read the text from the response and split into a javascript array
         function () {
             let ourData = JSON.parse(ourRequest.responseText);
             //call function to use image id clicked on to determine what data display
             let houseToDisplay = getSelectedHouse(ourData);
-            console.log(houseToDisplay);
+            //call function that creates and displays crad with house details
             displayHouseDetails(houseToDisplay);
         };
-    ourRequest.send();
 }
 
-//uses saved image element id clicked on to determine which listing needs to be displayed
+//uses saved image element id from image clicked on by user to determine which listing needs to be displayed
 function getSelectedHouse(data) {
     correctObject = false;
     for (let object of data) {
@@ -163,6 +158,9 @@ function getSelectedHouse(data) {
     }
     return selectedHouse;
 }
+
+//Creates the card element to house the selected houses details
+// takes single house object as an argument
 function displayHouseDetails(house) {
     document.getElementById("card-container").innerHTML = "";
     console.log(house.price);
